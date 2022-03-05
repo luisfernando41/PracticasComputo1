b8879509c024873cb5ee9bc499b9e3a3a72217a2
luisfernando41 <flechermartinez.41@gmail.com>
2022-03-04 20:23:01 -0600

    agrego archivos de la practica 3

---
+++ Practica3_eventos/firstapp/src/components/Firstcomponent.vue
@@ -0,0 +1,26 @@
+<template>
+   <div id="root">
+       <h1 v-on:mousemove="getXY">{{title}}</h1>
+       {{x}},{{y}}
+       </div> 
+</template>
+
+<script>
+export default{
+    name:'FirstComponent',
+    data(){
+        return{
+            title: 'Mi primer componente',
+            x:0,
+            y:0
+        }
+        },
+        methods:{
+            getXY(event){
+                this.x=event.offsetX;
+                this.y=event.offsetY;
+            }
+        }
+     }
+
+</script>
